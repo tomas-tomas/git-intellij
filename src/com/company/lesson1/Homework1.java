bd8014d338269b1078eeb3f473cd97f81fbb64d3
Samat <ergeshmametuulusamat@gmail.com>
2022-09-28 07:35:15 +0300

    Выполнил правки

--- src/com/company/lesson1/Homework1.java
+++ src/com/company/lesson1/Homework1.java
@@ -6,6 +6,6 @@ public class Homework1 {
     }
 
     private static void task1() {
-        System.out.println("Решение первой задачи");
+        System.out.println("Решение первой задачи!");
     }
 }
